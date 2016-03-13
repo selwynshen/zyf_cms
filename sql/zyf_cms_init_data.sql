c6592125dbab75c225cb9296cca08e3fac40bd05
selwynshen <selwynshen@gmail.com>
2016-03-13 13:56:13 +0800

    提交sql

---
+++ sql/zyf_cms_init_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `auth_group` (`id`, `name`) VALUES (1, 'admin');
+INSERT INTO `auth_group` (`id`, `name`) VALUES (3, 'branch shop');
+INSERT INTO `auth_group` (`id`, `name`) VALUES (2, 'head shop');
+
+INSERT INTO `auth_user_groups` VALUES ('1', '1', '1');
